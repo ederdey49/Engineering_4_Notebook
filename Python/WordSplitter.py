118f8bca7121a2eb8f84c2f1601d471aafa8871e
lcrosby13 <lcrosby13@charlottesvilleschools.org>
2019-08-30 11:36:31 -0400

    Adding my Strings and Loops program

---
+++ Python/WordSplitter.py
@@ -0,0 +1,17 @@
+#Strings and Loops
+#Code by Ned Derdeyn
+
+text = input("Type a sentence: ")
+
+wordArr = text.split()
+
+numWords = len(wordArr)
+
+for i in range(0, numWords):
+    word = wordArr[i]
+    numLetters = len(word)
+    for j in range(0, numLetters):
+        letter = word[j]
+        print(letter)
+    print("-")
+    
