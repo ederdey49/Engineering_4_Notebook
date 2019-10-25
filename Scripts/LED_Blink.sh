fdff56e33aececd290e66b2f4078c730bca1aac2
ederdey49 <54677503+ederdey49@users.noreply.github.com>
2019-10-25 11:27:57 -0400

    Update LED_Blink.sh

--- Scripts/LED_Blink.sh
+++ Scripts/LED_Blink.sh
@@ -1,16 +1,16 @@
-gpio mode 0 out
-gpio write 0 0
-gpio mode 1 out
-gpio write 1 0
+gpio mode 0 out #Set pin 0 to output
+gpio write 0 0 #Turn pin 0 off
+gpio mode 1 out #Set pin 1 to output
+gpio write 1 0 #Turn pin 1 off
 
-for i in {0..9}
+for i in {0..9} #Do this 10 times (0 to 9 inclusive)
 do
-	gpio write 0 1
-	gpio write 1 0
-	sleep .51s
-	gpio write 0 0
-	gpio write 1 1
-	sleep .51s
+	gpio write 0 1 #Turn pin 0 on
+	gpio write 1 0 #Turn pin 1 off
+	sleep .51s #Pause
+	gpio write 0 0 #Turn pin 0 off
+	gpio write 1 1 #Turn pin 1 on
+	sleep .51s #Pause
 done
 
-gpio write 1 0
+gpio write 1 0 #Turn pin 1 off
