c40a51f07c627680c0246981dc054e4702a760c1
lcrosby13 <54591964+lcrosby13@users.noreply.github.com>
2019-10-25 11:43:24 -0400

    Commented

--- Python/DiceRoller.py
+++ Python/DiceRoller.py
@@ -1,14 +1,12 @@
 #Automatic Dice Roller
 #Lucia Crosby and Ned Derdeyn
 
-from random import randint
+from random import randint #let us make random integers
 
-print("Automatic Dice Roller")
+print("Automatic Dice Roller") #print "automatic dice roller"
 
-value = input("Press ENTER to roll. Press X, then ENTER to quit.")
+value = input("Press ENTER to roll. Press X, then ENTER to quit.") #print instructions for how to run the game
 
-while (value == ""):
-    print(randint(1,6))
-    value = input()
-
-exit()
+while (value == ""): #while the user continues to input nothing
+    print(randint(1,6)) #print a number between 1 and 6
+    value = input() #another place to input nothing if the user wants to continue or literally anything else to quit
