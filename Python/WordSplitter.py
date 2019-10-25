34b045d01166dab1dc30d0d316d8dd4554e16331
ederdey49 <54677503+ederdey49@users.noreply.github.com>
2019-10-25 11:43:40 -0400

    Commented
    Also simplified because I did a few dumb things

--- Python/WordSplitter.py
+++ Python/WordSplitter.py
@@ -1,16 +1,12 @@
 #Strings and Loops
 #Code by Ned Derdeyn
 
-text = input("Type a sentence: ")
+text = input("Type a sentence: ") #prompts the user to type in an input sentence
 
-wordArr = text.split()
+wordArr = text.split() #make an array in which each element is one of the words from the input sentence
 
-numWords = len(wordArr)
-
-for i in range(0, numWords):
-    word = wordArr[i]
-    numLetters = len(word)
-    for j in range(0, numLetters):
-        letter = word[j]
-        print(letter)
-    print("-")
+for i in range(0, len(wordArr)): #do this until the counter i equals the number of words in wordArr
+    word = wordArr[i] #set the i-th element of wordArr to a new string
+    for j in range(0, len(word)): #do this until the counter j equals the number of letters in word
+        print(word[j]) #print the j-th letter of word
+    print("-") #print a dash between words to separate them
