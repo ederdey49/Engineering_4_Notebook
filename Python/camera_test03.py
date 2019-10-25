38dc15bba635150c9f7967db971b25042735feaa
ederdey49 <54677503+ederdey49@users.noreply.github.com>
2019-10-25 11:30:17 -0400

    Update camera_test03.py
    Commented

--- Python/camera_test03.py
+++ Python/camera_test03.py
@@ -1,11 +1,11 @@
-from picamera import PiCamera
-from time import sleep
+from picamera import PiCamera #get this object in here
+from time import sleep #this one too
 
-camera = PiCamera()
+camera = PiCamera() #create a camera object called "camera"
 
-camera.start_preview()
-sleep(1)
-camera.start_recording('/home/pi/Desktop/video.h264')
-sleep(10)
-camera.stop_recording()
-camera.stop_preview()
+camera.start_preview() #open the camera window on the screen
+sleep(1) #pause for 1 second
+camera.start_recording('/home/pi/Desktop/video.h264') #start recording to a file at this location
+sleep(10) #pause for 10 seconds
+camera.stop_recording() #stop recording
+camera.stop_preview() #close the camera window
