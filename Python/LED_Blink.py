8312422289ea052161e6ce6eb4680f4b02afded0
lcrosby13 <54591964+lcrosby13@users.noreply.github.com>
2019-10-28 11:37:36 -0400

    Commented

--- Python/LED_Blink.py
+++ Python/LED_Blink.py
@@ -1,18 +1,18 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO #libraries stuff
 
 from time import sleep
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT, initial = GPIO.LOW)
-GPIO.setup(18, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setwarnings(False) #don't warn me i know what i'm doing
+GPIO.setmode(GPIO.BCM) #setup
+GPIO.setup(17, GPIO.OUT, initial = GPIO.LOW) #first led set up, starts off
+GPIO.setup(18, GPIO.OUT, initial = GPIO.LOW) #same
 
-for i in range(0,9):
-    GPIO.output(17, GPIO.HIGH)
-    GPIO.output(18, GPIO.LOW)
-    sleep(.5)
-    GPIO.output(17, GPIO.LOW)
-    GPIO.output(18, GPIO.HIGH)
-    sleep(.5)
+for i in range(0,9): #do this 9 times
+    GPIO.output(17, GPIO.HIGH) #turn led 1 on
+    GPIO.output(18, GPIO.LOW) #led 2 off
+    sleep(.5) #pause for 1/2 second
+    GPIO.output(17, GPIO.LOW) #led 1 off
+    GPIO.output(18, GPIO.HIGH) #led 2 on
+    sleep(.5) #pause for 1/2 second
 
-GPIO.output(18, GPIO.LOW)
+GPIO.output(18, GPIO.LOW) #turn led 2 off
