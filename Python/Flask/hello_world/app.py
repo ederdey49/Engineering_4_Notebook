cc8840372a9c5c16520169d9b2ac513a249e2bcf
ederdey49 <54677503+ederdey49@users.noreply.github.com>
2019-10-29 11:37:57 -0400

    Commented

--- Python/Flask/hello_world/app.py
+++ Python/Flask/hello_world/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask #import the Flask object from the flask directory
 
-app = Flask(__name__)
+app = Flask(__name__) #make a Flask object called app
 
-@app.route("/")
-def hello_world():
+@app.route("/") #at the root directory,
+def hello_world(): #this is the function to be run
 	return "hello world!"
 
 if __name__ == "__main__":
-	app.run(host= "0.0.0.0", port = 80)
+	app.run(host= "0.0.0.0", port = 80) #run the app so it's visible to outside computers
+	
+#this file interacts with the HTML file to create a webpage that you can visit at the pi's IP address
