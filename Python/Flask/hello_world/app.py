b0410e32d92bc32a44a975cc5bc5674024bddde5
ederdey49 <54677503+ederdey49@users.noreply.github.com>
2019-10-29 11:38:37 -0400

    Update app.py

--- Python/Flask/hello_world/app.py
+++ Python/Flask/hello_world/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__) #make a Flask object called app
 
 @app.route("/") #at the root directory,
 def hello_world(): #this is the function to be run
-	return "hello world!"
+	return "hello world!" #all it does is print this text
 
 if __name__ == "__main__":
 	app.run(host= "0.0.0.0", port = 80) #run the app so it's visible to outside computers
